e225c64d7bdd2d853f2a95941c1b038f1071a4c5
rgubbala11 <ravitejrajeev@gmail.com>
2018-01-14 15:06:20 +0530

    Added JSON Data
    Added SON Data in file and displayed in modal window

--- scripts/app.js
+++ scripts/app.js
@@ -22,3 +22,36 @@ function loadPage(page){
     
 }
 
+function loadBook(){
+    //document.getElementById("book_content").innerHTML='<object class="pageContainer" type="text/html" height="100%" width="100%" data="/Pages/LoadBook.html" ></object>';
+}
+
+function loadQuote(){
+  var myBookId = $(".quote").data('id');
+  readData(myBookId);
+  
+}
+
+function loadDataInModal(result){
+    $("#myModalForText").modal("show");
+    
+    $("#text_content").html(result);
+    
+}
+
+function readData(index){
+    var result = {};
+    $.getJSON('/data/quotes.json', function(data) {
+        for(var i=0; i< data.length; i++){
+            if(index == data[i].Id){
+                result = data[i];
+                var content = '<h1> ' + result.Title + " </h1> <br />";
+                content = content + "<div> " + result.Content + "</div>";
+                loadDataInModal(content);
+            }
+        }
+    });
+}
+
+
+
